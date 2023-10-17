0149cf7a559ada9cee40bead27c51ea7d7ab046e
Maria Kazachenok <m.kazachenok@intetics.com>
2023-10-17 16:23:40 +0300

    update cv

---
+++ assets/js/main.js
@@ -0,0 +1,260 @@
+/* Define all the functions */
+const setTitle = data => {
+    // Set page title
+    document.querySelector('#profileName').innerHTML = data.name;
+    document.querySelector('#profileSubTitle').innerHTML = data.sub_title;
+
+    const overviewList = document.querySelector('#overview');
+
+    data.about.overview.forEach((item) => {
+        const li = document.createElement('li');
+
+        const summaryItem = document.createElement('div');
+        summaryItem.className = 'ach-item';
+        summaryItem.innerHTML = item;
+
+        li.appendChild(summaryItem);
+        overviewList.appendChild(li);
+    });
+};
+
+const setLinks = links => {
+    const linksList = document.querySelector('#linksList');
+    links.forEach(link => {
+        const li = document.createElement('li');
+        li.className = "contact-list-item";
+        const a = document.createElement('a');
+        a.target = '_blank';
+        a.href = link.src;
+        a.textContent = link.value;
+
+        const span = document.createElement('span');
+        span.className = "contact-info";
+        const i = document.createElement('i');
+        i.className = link.class;
+        i.style = "min-width: 20px;";
+
+        span.appendChild(i);
+        a.prepend(span);
+        li.appendChild(a);
+
+        linksList.appendChild(li);
+    });
+};
+
+const setEducation = education => {
+    const listDiv = document.querySelector('#educationList');
+    education.forEach(edu => {
+        const div = document.createElement('div');
+
+        const eduHeader = document.createElement('div');
+        eduHeader.className = 'edu-header';
+        eduHeader.innerHTML = edu.name;
+        div.appendChild(eduHeader);
+
+        const eduDuration = document.createElement('div');
+        eduDuration.className = 'edu-duration';
+        eduDuration.innerHTML = edu.duration;
+        div.appendChild(eduDuration);
+
+        const eduClassData = document.createElement('div');
+        eduClassData.className = 'edu-class-data';
+
+        const eduStd = document.createElement('span');
+        eduStd.className = 'edu-std';
+        eduStd.innerHTML = edu.std;
+        eduClassData.appendChild(eduStd);
+        div.appendChild(eduClassData);
+
+        listDiv.appendChild(div);
+    });
+};
+
+const setProjects = projects => {
+    const ul = document.querySelector('#projectList');
+    projects.forEach(project => {
+        const li = document.createElement('li');
+
+        const projectHeader = document.createElement('div');
+        projectHeader.className = 'project-header';
+
+        const projectTitle = document.createElement('span');
+        projectTitle.className = 'project-title';
+        projectTitle.innerHTML = project.title;
+        projectHeader.appendChild(projectTitle);
+
+        const projectOrganiz = document.createElement('span');
+        projectOrganiz.className = 'organiz-name';
+        projectOrganiz.innerHTML = project.organization;
+        projectHeader.appendChild(projectOrganiz);
+
+        li.appendChild(projectHeader);
+
+        const projectDuration = document.createElement('span');
+        projectDuration.className = 'project-duration';
+        projectDuration.innerHTML = project.duration;
+        li.appendChild(projectDuration);
+
+        const projectDomain = document.createElement('div');
+        projectDomain.className = 'project-domain';
+        projectDomain.innerHTML = project.domain;
+
+        li.appendChild(projectDomain);
+
+        const projectDesc = document.createElement('ul');
+        projectDesc.className = 'project-desc';
+
+        project.responsabilities.forEach(prResp => {
+            const resp = document.createElement('li');
+            resp.className = 'project-desc-item';
+            resp.innerHTML = prResp;
+            projectDesc.appendChild(resp);
+        })
+
+        li.appendChild(projectDesc);
+
+        const techCont = document.createElement('div');
+        techCont.className = 'project-tech-cont';
+        li.appendChild(techCont);
+
+        project.technologies.forEach(topic => {
+            const projectTech = document.createElement('div');
+            projectTech.className = 'project-tech';
+            projectTech.innerHTML = topic;
+            techCont.appendChild(projectTech);
+        })
+
+        ul.appendChild(li);
+    });
+};
+
+const setSkills = skills => {
+    const skillList = document.querySelector('#skillList');
+    skills.forEach(skill => {
+        let type_klass = 'cat-skill-type force-inline';
+        let body_klass = 'cat-skill-body-gen';
+
+        const catSkillItem = document.createElement('li');
+        catSkillItem.className = 'cat-skill-item';
+
+        const catSkillType = document.createElement('div');
+        catSkillType.className = type_klass;
+        catSkillType.innerHTML = skill.category;
+        catSkillItem.appendChild(catSkillType);
+
+        const catSkillBody = document.createElement('div');
+        catSkillBody.className = body_klass;
+
+        skill.topics.forEach(topic => {
+            const skillGen = document.createElement('div');
+            skillGen.className = 'skill-gen';
+            skillGen.innerHTML = topic;
+            catSkillBody.appendChild(skillGen);
+        });
+
+        catSkillItem.appendChild(catSkillBody);
+
+        skillList.appendChild(catSkillItem);
+    });
+};
+
+const setCertification = certifications => {
+    const certList = document.getElementById('certList');
+
+    certifications.forEach(evt => {
+        const li = document.createElement('li');
+
+        const achItem = document.createElement('div');
+        achItem.className = 'ach-item';
+
+        const achTitle = document.createElement('div');
+        achTitle.className = 'ach-title';
+        achTitle.innerHTML = evt.desc;
+        achItem.appendChild(achTitle);
+
+        const achDuration = document.createElement('div');
+        achDuration.className = 'ach-duration';
+        achDuration.innerHTML = evt.date;
+        achItem.appendChild(achDuration);
+
+        li.appendChild(achItem);
+        certList.appendChild(li);
+    });
+};
+
+const setCatagoryHeader = title => {
+    const catHeader = document.createElement('div');
+    catHeader.className = 'cat-header';
+
+    const catIndicator = document.createElement('span');
+    catIndicator.className = 'cat-indicator';
+
+    const i = document.createElement('i');
+    i.className = 'fa fa-chevron-right';
+    i['area-hidden'] = 'true';
+    catIndicator.appendChild(i);
+    catHeader.appendChild(catIndicator);
+
+    const catTitle = document.createElement('span');
+    catTitle.className = 'cat-title';
+    catTitle.innerHTML = title;
+    catHeader.appendChild(catTitle);
+
+    return catHeader;
+};
+
+const setHobbies = hobbies => {
+    const list = document.getElementById('hobbies');
+
+    hobbies.forEach(evt => {
+        const li = document.createElement('li');
+
+        const achItem = document.createElement('div');
+        achItem.className = 'ach-item';
+
+        const achTitle = document.createElement('div');
+        achTitle.className = 'ach-title';
+        achTitle.innerHTML = evt;
+        achItem.appendChild(achTitle);
+
+        li.appendChild(achItem);
+        list.appendChild(li);
+    })
+}
+
+const setLanguages = languages => {
+    const list = document.getElementById('languages');
+
+    languages.forEach(evt => {
+        const li = document.createElement('li');
+
+        const achItem = document.createElement('div');
+        achItem.className = 'ach-item';
+
+        const achTitle = document.createElement('div');
+        achTitle.className = 'ach-title';
+        achTitle.innerHTML = evt.name;
+        achItem.appendChild(achTitle);
+
+        const achDuration = document.createElement('div');
+        achDuration.className = 'ach-duration';
+        achDuration.innerHTML = evt.level;
+        achItem.appendChild(achDuration);
+
+        li.appendChild(achItem);
+        list.appendChild(li);
+    })
+}
+
+//  Entry Function, IIFE
+(() => {
+    // Call functions to load profile
+    setTitle(profileData);
+    setLinks(profileData.links);
+    setProjects(profileData.projects);
+    setSkills(profileData.skills);
+    setEducation(profileData.education);
+    setCertification(profileData.certifications);
+    setLanguages(profileData.languages);
+    setHobbies(profileData.hobbies);
+})();
---
+++ data/profile.js
@@ -0,0 +1,186 @@
+const profileData = {
+    title: "Resume",
+    name: "Maria Kazachenok",
+    sub_title: "Senior Full Stack Software Engineer",
+    about: {
+        overview: [
+            'Full stack software engineer adept in working in both front-end and back-end development processes within 8.5 years.',
+            'Highly skilled in development in programming design, development and implementation of functional specifications.',
+            'Possesses a strong command of multiple programming languages and technologies, including C#, T-SQL, .NET, .NET Core, ASP.NET Core, EF Core, MS SQL, Typescript, Angular, React and Azure DevOps.',
+            'Has good understanding of all stages of software development process.',
+            'Has an experience with Microsoft Azure, proven by having passed the Microsoft Certified Azure Developer Associate certification.',
+            'Excellent soft skills, team player.'
+        ],
+    },
+    links: [
+        { title: "Phone", class: "fas fa-phone", src: "", value: "+375297412852" },
+        { title: "Location", class: "fa fa-map-marker", src: "https://maps.app.goo.gl/422Mq7zGixSBZz186", value: "Minsk, Belarus" },
+        { title: "Email", class: "fa fa-envelope", src: "mailto:kazachenokm@gmail.com", value: "kazachenokm@gmail.com" },
+        { title: "Leetcode", class: "fas fa-code", src: "https://leetcode.com/mkazachenok/", value: "leetcode.com/mkazachenok" },
+        { title: "Github", class: "fab fa-github-alt", src: "https://github.com/MashaKazachenok", value: "github.com/MashaKazachenok" },
+        { title: "LinkedIn", class: "fab fa-linkedin-in", src: "https://www.linkedin.com/in/maria-kazachenok-b89044b2/", value: "linkedin.com/in/maria-kazachenok-b89044b2" },
+    ],
+    education: [
+        {
+            name: "Belorussian National Technical University",
+            duration: "2010 - 2015",
+            std: "Information technology and robotics",
+        }
+    ],
+    skills: [
+        {
+            category: "Backend",
+            topics: ["C#", ".Net MVC", ".Net Core", ".Net Framework", "ASP.Net", "EF", "Nhirernate", "Dapper", "Quartz",
+                "WebAPI", "RabbitMQ", "NUnit", "Selenium Tests"]
+        },
+        {
+            category: "Frontend",
+            topics: ["Javascript", "Typescript", "Angular", "ReactJs", "Redux", "Mobx", "VueJs", "CSS", "LESS", "jQuery",
+                "Material UI", "Ant Design", "Bootstrap", "PrimeNG", "Kendo UI"]
+        },
+        {
+            category: "Mobile Dev",
+            topics: ["React Native", "Ionic", "Capacitor"]
+        },
+        {
+            category: "Tools",
+            topics: ["MS Visual Studio", "MS Visual Studio Code", "Swagger", "Postman", "XCode", "Android Studio", "Figma", "AzCopy",
+                "MS SQL Management Studio"]
+        },
+        {
+            category: "Databases",
+            topics: ["MS SQL", "MySQL", "MongoDB"]
+        },
+        {
+            category: "DevOps",
+            topics: ["CI", "CD", "Azure DevOps", "TeamCity", "Jenkins", "Git", "App Store", "Play Market"]
+        },
+        {
+            category: "Microsoft Azure",
+            topics: ["Azure SQL", "Azure Blob Storage", "Azure VMs", "Azure Load Balancer", "CDN", "Azure App Services"]
+        }
+    ],
+    projects: [
+        {
+            organization: "Intetics, Inc.",
+            title: "Full stack .NET | React Developer",
+            duration: "November 2021 - October 2023",
+            domain: "Data conversion, 7 members",
+            responsabilities: [
+                'Writing tools to download data from sites, convert HTML to XML, collect different types of images and uploading them to databases for further use.',
+                'Creating db scripts and excel reports.',
+                'Merging Databases issues.',
+                'Writing new and updating existing Web tools helping to process data.',
+                'Developing process for automatical data conversion using RabbitMQ.',
+                'Code review, performance optimization, mentoring.'
+            ],
+            achievements: ``,
+            technologies: [".NET", ".NET Core", ".NET Core WEB API", "EF Core", "C#", "T-SQL", "MS SQL", "RabitMQ", "Hangfire", "ReactJS", "Redux", "MobX", "Ant Design", "Bootstrap", "Git", "SVN", "Log4net"]
+        },
+        {
+            organization: "Intetics, Inc.",
+            title: "Full stack .NET | Angular Developer",
+            duration: "July 2020 - November 2021",
+            domain: "E-Learning, 8 members",
+            responsabilities: [
+                'Implementing back-end functionality and business logic.',
+                'Implementing responsible screen size Web and mobile front-end UI and logic.',
+                'Working with DB. Export jobs implementation. Import tools developing.',
+                'Code reviewing, refactoring legasy functionality.',
+                'Setting CI/CD in Azure Devops.',
+                'Working with Microsoft Azure services: setting up databases, virtual machines, blob storage, CDN and Load Balancer.',
+                'Building mobile .ipa and .apk files using XCode and Android Studio, sending it to App store and Play market.'],
+            achievements: ``,
+            technologies: [".NET Framework", ".NET Core", "ASP.NET MVC", "ASP.NET Core WEB API", "Azure Blob Storage", "Azure VMs", "Azure DevOps Services", "AzCopy",
+                "Identity Server 4", "NHibernate", "Quartz", "MS SQL", "Angular 10", "TypeScript", "Material UI", "Ionic", "Capacitor", "Swagger", "PaygateAPI", "Log4net", "Test Flight"]
+        },
+        {
+            organization: "Intetics, Inc.",
+            title: "Full stack .NET | Angular Developer",
+            duration: "November 2019 - June 2020",
+            domain: "Health Care, 5 members",
+            responsabilities: [
+                'Development front-end and back-end functionality.',
+                'Architecture tasks. work with DB, daily jobs implementation. Code reviewing.',
+                'Iteraction with other APIs.',
+                'Deployment and authorization issues.',
+                'Writing Unit tests.'
+            ],
+            achievements: ``,
+            technologies: [".NET Core WEB API", "MS SQL", "MongoDB", "Dapper", "Angular9", "Ionic", "Capacitor", "nginx", "Swagger", "Firebase",
+                "Quartz", "NLog"]
+        },
+        {
+            organization: "Intetics, Inc.",
+            title: "Frontend VueJS Developer",
+            duration: "August 2019 - November 2019",
+            domain: "Adverta, 5 members",
+            responsabilities: [
+                'Developing front-ent part of the application, including responsive design and rtl support.',
+                'Implementing landing page.',
+                'Implementing front-end localization.',
+                'Working with Docker and MariaDB.'
+            ],
+            achievements: ``,
+            technologies: ["VueJS", "Vuex", "Vuetify", "Postman", "Docker", "MariaDB", "Figma"]
+        },
+        {
+            organization: "Intetics, Inc.",
+            title: "Mobile React Native Developer",
+            duration: "December 2018 - April 2019",
+            domain: "Health Care, 4 members",
+            responsabilities: [
+                'Developing and building front-end side of the application from scratch to the final version.',
+                'Data structure design, integration with backend API.',
+                'Postman testing.'
+            ],
+            achievements: ``,
+            technologies: ["React Native", "Javascript", "Postman", "MySQL"]
+        },
+        {
+            organization: "Intetics, Inc.",
+            title: "Full stack .NET | Angular Developer",
+            duration: "July 2018 - November 2018",
+            domain: "Workflow Management System, 5 members",
+            responsabilities: [
+                'Designing and developing functionality and business logic.',
+                'Implementing back-end and front-end localization.',
+            ],
+            achievements: ``,
+            technologies: ["C#", ".NET Core", "MySQL", "Angular6", "Material UI", "NHibernate", "Serilog", "Git", "FluentMigrator"]
+        },
+        {
+            organization: "Intetics, Inc.",
+            title: "Full stack .NET Developer",
+            duration: "August 2015 - June 2018",
+            domain: `E-Learning, 3-15 team members`,
+            responsabilities: [
+                'Development of new and extending existing functionality and business logic.',
+                'Making report using Rdlc Viewer.',
+                'Work with DB. Performance improvements. Code review.',
+                'Writing Unit and Selenium tests.'],
+            achievements: ``,
+            technologies: [".NET", "C#", "ASP.NET MVC", "SQL Server", "NHibernate", "NUnit", "Bootstrap", "TeamCity",
+                "Jazascript", "JQuery", "KendoUI", "Dapper", "CSS", "SVN"]
+        }
+    ],
+    certifications: [
+        {
+            desc: `<strong>Microsoft Certified: Azure Developer Associate</strong>.
+        (<a target='_blank' rel='noreferrer' href='https://www.credly.com/badges/1fe7b2ae-d029-4a32-9e28-dce465ac89db'>bit.ly/46NvsaB</a>)`,
+            date: "Febrary 2022"
+        },
+        {
+            desc: `<strong>Microsoft Certified: Azure Fundamentals</strong>.
+         (<a target='_blank' rel='noreferrer' href='https://www.credly.com/badges/b88fef1b-72da-4e3b-ab67-d93f66a2228b'>bit.ly/3QeP55P</a>)`,
+            date: "April 2021"
+        }
+    ],
+    hobbies: ["Travelling", "Running", "Snowboarding", "Reading", "Language Learning"],
+    languages: [
+        { name: "Russian", level: "C2" },
+        { name: "English", level: "B2" },
+        { name: "French", level: "A2" },
+        { name: "Spanish", level: "A2" }],
+    events: []
+};
